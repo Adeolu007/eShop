71ea1b988422cfea480ed36b86554b666e261af5
Adeolu.Odunuyi <odunuyiadeolu@gmail.com>
2024-07-09 14:22:57 +0100

    made alterations to order routes and added some methods

--- routers/orders.js
+++ routers/orders.js
@@ -7,14 +7,33 @@ const { OrderItem } = require('../models/order-item');
 
 
 
-router.get(`/`, async(req, res)=>{
-    const orderList = await Order.find();
+router.get(`/`, async (req, res) =>{
+    // .populate is used to populate the details of a table (in this context, it will polupate the details of the user when returning the value instead of only the id)
+    // .sort({'dateOrdered': -1}) is used to sort from newest to oldest
+    const orderList = await Order.find().populate('user', 'name').sort({'dateOrdered': -1});
 
-    if(!orderList){
-        res.status(500).json({success:false})
-    }
-    res.send(orderList)
+    if(!orderList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(orderList);
 })
+
+router.get(`/:id`, async (req, res) =>{
+    const order = await Order.findById(req.params.id)
+    .populate('user', 'name')
+    .populate({ 
+        // path is used to populate orseItms because it is in a nested Array, same with product
+        // the key('orderItems') is the name of the array in which the second one(what you want to populate) is inside, and the the second (populate: 'category') is what you want to populate
+        path: 'orderItems', populate: {
+            path : 'product', populate: 'category'} 
+        });
+
+    if(!order) {
+        res.status(500).json({success: false})
+    } 
+    res.send(order);
+})
+
 
 router.post('/', async (req,res)=>{
     const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) =>{
@@ -55,6 +74,76 @@ router.post('/', async (req,res)=>{
     return res.status(400).send('the order cannot be created!')
 
     res.send(order);
+})
+
+router.put('/:id',async (req, res)=> {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status
+        },
+        { new: true}
+    )
+
+    if(!order)
+    return res.status(400).send('the order cannot be update!')
+
+    res.send(order);
+})
+
+
+router.delete('/:id', (req, res)=>{
+    Order.findByIdAndRemove(req.params.id).then(async order =>{
+        if(order) {
+            await order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            })
+            return res.status(200).json({success: true, message: 'the order is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "order not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+//to get total order sales
+router.get('/get/totalsales', async (req, res)=> {
+    const totalSales= await Order.aggregate([
+        { $group: { _id: null , totalsales : { $sum : '$totalPrice'}}}
+    ])
+
+    if(!totalSales) {
+        return res.status(400).send('The order sales cannot be generated')
+    }
+
+    res.send({totalsales: totalSales.pop().totalsales})
+})
+
+
+// count all orders
+router.get(`/get/count`, async (req, res) =>{
+    const orderCount = await Order.countDocuments((count) => count)
+
+    if(!orderCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        orderCount: orderCount
+    });
+})
+
+// Get all orders from single user
+router.get(`/get/userorders/:userid`, async (req, res) =>{
+    const userOrderList = await Order.find({user: req.params.userid}).populate({ 
+        path: 'orderItems', populate: {
+            path : 'product', populate: 'category'} 
+        }).sort({'dateOrdered': -1});
+
+    if(!userOrderList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(userOrderList);
 })
 
 
